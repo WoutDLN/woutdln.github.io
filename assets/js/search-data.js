ffc89b42c9f73fc816600f0b921f7b5c8e0a395c
WoutDLN <7235631+WoutDLN@users.noreply.github.com>
2025-11-29 18:25:21 +0000

    Deploying to gh-pages from @ WoutDLN/woutdln.github.io@aa78398e744ad30209713d3337064386a6eae1cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -529,13 +529,6 @@ ninja.data = [{
           window.open("https://github.com/WoutDLN", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-orcid',
         title: 'ORCID',
         section: 'Socials',
